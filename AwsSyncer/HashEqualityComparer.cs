ecd26e112fa8b51f1bfc9d20879278e452d683a8
henric <software@henric.org>
2016-03-17 23:45:13 -0700

    Add HasEqualityComparer.

---
+++ AwsSyncer/HashEqualityComparer.cs
@@ -0,0 +1,63 @@
+// Copyright (c) 2016 Henric Jungheim <software@henric.org>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsSyncer
+{
+    /// <summary>
+    ///     Generate a quick hash code for byte arrays that are known
+    ///     to be uniformly random.
+    /// </summary>
+    public sealed class HashEqualityComparer : IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[] x, byte[] y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (ReferenceEquals(x, null) || ReferenceEquals(null, y))
+                return false;
+
+            if (x.Length != y.Length)
+                return false;
+
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(byte[] obj)
+        {
+            if (obj.Length >= sizeof(int))
+                return BitConverter.ToInt32(obj, 0);
+
+            var code = 0;
+
+            foreach (var v in obj)
+            {
+                code <<= 8;
+                code |= v;
+            }
+
+            return code;
+        }
+    }
+}
